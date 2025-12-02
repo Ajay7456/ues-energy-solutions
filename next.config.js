06face454bb9bb4143fc6f39e573a542aa5bbb4b
Ajay7456 <66772362+Ajay7456@users.noreply.github.com>
2025-12-02 20:06:07 +0000

    okay

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '**',
+      },
+    ],
+  },
   reactStrictMode: true,
 }
 
